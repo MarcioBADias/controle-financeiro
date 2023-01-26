341cea37126bd4f9e4d277c440cafaf65efc24da
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-01-26 05:41:27 -0300

    IMplementando o FORM

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import * as C from './styles';
 
 const Form = () => {
+    const [desc, setDesc] = useState('');
+    const [amount, setAmount] = useState('');
+    const [isExpense, setExpense] = useState('');
+
+    const handleSave = () => {
+        if (!desc || !amount){
+            alert('Informe a descrição e o valor');
+            return;
+        } else if (amount < 1) {
+            alert('O valor tem que ser positivo!');
+            return;
+        };
+    };
+
     return (
-        <C.Container>Form para receber entradas ou saídas</C.Container>
+        <C.Container>
+            <C.ImputContent>
+                <C.Label>Descrição</C.Label>
+                <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+            </C.ImputContent>
+            <C.ImputContent>
+                <C.Label>Valor</C.Label>
+                <C.Input value={amount} type='number' onChange={(e) => setAmount(e.target.value)} />
+            </C.ImputContent>
+            <C.RadioGroup>
+                <C.Input type='radio' id='rIncome' defaultChecked name='group1' onChange={() => setExpense(!isExpense)} />
+                <C.Label htmlFor='rIncome'>Entrada</C.Label>
+                <C.Input type='radio' id='rExpenses' name='group1' onChange={() => setExpense(!isExpense)} />
+                <C.Label htmlFor='rExpenses'>Saída</C.Label>
+            </C.RadioGroup>
+            <C.Button onCLick={handleSave}>ADICIONAR</C.Button>
+        </C.Container>
     )
 }
 
