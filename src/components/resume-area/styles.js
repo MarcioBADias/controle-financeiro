13d968550d3c959689cfd7bc8c71858057543a7c
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-01-28 20:30:48 -0300

    Ajuste do resume area

--- src/components/resume-area/styles.js
+++ src/components/resume-area/styles.js
@@ -4,11 +4,11 @@ export const Table = styled.table`
     background-color: #fbf2a2;
     border-radius: 10px 10px 0 0;
     color: #333;
-    margin: 3rem auto 0;
+    margin: 3rem auto 0 auto;
     padding: 1rem;
     max-width: 1024px;
     text-align: center;
-
+    width: 90vw;
 
 `;
 
